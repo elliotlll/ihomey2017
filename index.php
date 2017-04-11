9c5a789cd97d80b5270005be880b585923cffb2a
wangli <wangli84@gmail.com>
2017-04-11 10:43:38 +0800

    初始化

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>家和作用 - 创造情境照明新体验，专注LED智能落地灯</title>
+    <meta name="keywords" content="家和作用 - 创造情境照明新体验，专注LED智能落地灯" />
+    <meta name="description" content="家和作用经过长达三年的产品开发，摒弃了将近20款左右产品后，奠定了以“绝对原创、成熟光源、交互体验、轻型智能家居”为无可置疑的产品设计导向，同时从中也获得了对公司管理、服务以及团队培养的基本原则。" />
+    <link rel="stylesheet" href="/css/css.css">
+</head>
+<body>
+    <div class="container indexbg">
+        <div class="language">
+            <span class="en"><a href="/en">English</a></span>
+            <span class="cn"><a href="/cn">中文</a></span>
+        </div>
+    </div>
+    <script src="js/jquery-1.11.1.min.js"></script>
+</body>
+</html>
