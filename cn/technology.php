9a31b1bf57d7b30fda6b20d6489520a06d2566d1
elliot <wangli84@gmail.com>
2017-09-13 21:45:02 +0800

    修复中英文

--- cn/technology.php
+++ cn/technology.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Technology iHomey</title>
+        <title>科技-家和作用</title>
         <meta name="keywords" content="家和作用 - 创造情境照明新体验，专注LED智能落地灯" />
         <meta name="description" content="家和作用经过长达三年的产品开发，摒弃了将近20款左右产品后，奠定了以“绝对原创、成熟光源、交互体验、轻型智能家居”为无可置疑的产品设计导向，同时从中也获得了对公司管理、服务以及团队培养的基本原则。" />
         <link href="/css/css.css" rel="stylesheet">
